a3e46dee6602db42e9f32a2476a87e06c60bec2f
Daniel Yohan <dyohan9@gmail.com>
2020-10-19 14:16:11 -0300

    fix pep8

--- pythongram/__init__.py
+++ pythongram/__init__.py
@@ -3,4 +3,4 @@ from pythongram import version
 # define the version before the other imports since these need it
 __version__ = version.__version__
 
-from pythongram import pythongram
+from pythongram import pythongram  # noqa: F401
